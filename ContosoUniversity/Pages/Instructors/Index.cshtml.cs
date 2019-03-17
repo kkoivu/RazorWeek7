45ac90425db622023d0e466a04c3e400b31cbf42
Karl-Erik Kõivupuu <modernkarl@hotmail.com>
2019-03-17 17:14:39 +0200

    Scaffolded the instructor model and updated the instructors index page

---
+++ ContosoUniversity/Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using ContosoUniversity.Models;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;  // Add VM
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Pages.Instructors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SchoolContext _context;
+
+        public IndexModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public InstructorIndexData Instructor { get; set; }
+        public int InstructorID { get; set; }
+
+        public async Task OnGetAsync(int? id)
+        {
+            Instructor = new InstructorIndexData();
+            Instructor.Instructors = await _context.Instructors
+                  .Include(i => i.OfficeAssignment)
+                  .Include(i => i.CourseAssignments)
+                    .ThenInclude(i => i.Course)
+                  .AsNoTracking()
+                  .OrderBy(i => i.LastName)
+                  .ToListAsync();
+
+            if (id != null)
+            {
+                InstructorID = id.Value;
+            }
+        }
+    }
+}
